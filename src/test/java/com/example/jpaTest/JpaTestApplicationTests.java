dbee0d13d616fd46725abbf5a24ca63237978d9d
jaydenchuljinlee <cheoljin1234@naver.com>
2020-05-09 13:04:12 +0900

    20200509 create project

---
+++ src/test/java/com/example/jpaTest/JpaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
